9b87af5bf6e25430c6195cf17832ed5745e96f66
Maxim Veksler <maximvek05@gmail.com>
2024-12-05 11:12:28 -0500

    updated demo

--- static/js/take_photo.js
+++ static/js/take_photo.js
@@ -22,9 +22,11 @@ function takePictureVision() {
     //     } else {
     // let keys = Object.keys(data.message);
     // for (let i = 0; i < keys.length; i++) {
-    let objects = ["apple", "banana", "pear"];
-    addItem(objects[num], 1);
-    num++;
+    setTimeout(() => {
+        let objects = ["apple", "banana", "pear"];
+        addItem(objects[num], 1);
+        num++;
+    }, 1500);
     // }
     //     }
     // })
