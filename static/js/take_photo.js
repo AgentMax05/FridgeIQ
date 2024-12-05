fe208568d47d9cce586ea3c118ba9cf71b4c860f
Maxim Veksler <maximvek05@gmail.com>
2024-12-05 11:13:24 -0500

    fixed capitalization

--- static/js/take_photo.js
+++ static/js/take_photo.js
@@ -23,7 +23,7 @@ function takePictureVision() {
     // let keys = Object.keys(data.message);
     // for (let i = 0; i < keys.length; i++) {
     setTimeout(() => {
-        let objects = ["apple", "banana", "pear"];
+        let objects = ["Apple", "Banana", "Pear"];
         addItem(objects[num], 1);
         num++;
     }, 1500);
